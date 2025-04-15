1ed45241dc343735f44364259e415cd0036ae050
Rushit Thummar <rushitthummar118@gmail.com>
2025-04-15 17:59:41 +0530

    Add files via upload

---
+++ useEffect()-Joker/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { sum } from "./helper.js";
+import Form from "./Form.jsx";
+import CommentsForm from "./CommentsForm.jsx";
+import Comment from "./Comment.jsx";
+import Joker from "./Joker.jsx";
+
+function App() {
+  return (
+    <>
+      <Joker />
+    </>
+  );
+}
+
+export default App;
